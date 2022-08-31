af3df851ee124d03613a1568421f9d26a5f793e9
baky30 <amanbakiev99@gmail.com>
2022-09-01 00:20:58 +0400

    fix SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStack } from "./src/navigation/MainStack";
 import { Provider } from "react-redux";
 import { persistor, store } from "./src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <SafeAreaView className={"flex-1"}>
-            <StatusBar backgroundColor={"white"} barStyle={"dark-content"} />
-            <MainStack />
-          </SafeAreaView>
-        </NavigationContainer>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <SafeAreaView className={"flex-1 bg-white"} edges={["top"]}>
+              <StatusBar backgroundColor={"white"} barStyle={"dark-content"} />
+              <MainStack />
+            </SafeAreaView>
+          </NavigationContainer>
+        </SafeAreaProvider>
       </PersistGate>
     </Provider>
   );
